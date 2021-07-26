9d521f0aba7911e7f95c03f96557bc0cfb1a7c0f
Guilder W. Milliner <guilderw@live.com>
2021-07-25 18:10:44 -0600

    Add project files.

---
+++ SugarMonkey/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SugarMonkey
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
