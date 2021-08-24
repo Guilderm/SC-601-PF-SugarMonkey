9017c8512bb02fdf39064e16f24056909078b977
jjguzme <jjguzmanmeza15@gmail.com>
2021-08-24 13:18:44 -0600

    Product view added again

--- SugarMonkey/Controllers/ProductController.cs
+++ SugarMonkey/Controllers/ProductController.cs
@@ -1,13 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using SugarMonkey.Models;
+using SugarMonkey.Models.BusinessLogic;
+using System.Web.Mvc;
 
 namespace SugarMonkey.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
+        ProductBusinessLogic modelo = new ProductBusinessLogic();
+
+        [HttpGet]
         public ActionResult Product()
         {
-            return View();
+            ViewBag.ProductFilter = modelo.ListaCategoryBag();
+
+            return View(modelo.ConsultarProducts("0"));
+        }
+
+        [HttpPost]
+        public ActionResult ConsultarProducts(Product obj)
+        {
+            ViewBag.ProductFilter = modelo.ListaCategoryBag();
+
+            return View("Product", modelo.ConsultarProducts(obj.CategoryID.ToString()));
         }
     }
 }
--- SugarMonkey/Models/BusinessLogic/ProductBusinessLogic.cs
+++ SugarMonkey/Models/BusinessLogic/ProductBusinessLogic.cs
@@ -1,6 +1,33 @@
-﻿namespace SugarMonkey.Models.BusinessLogic
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SugarMonkey.Models.BusinessLogic
 {
     public class ProductBusinessLogic
     {
+        public List<Product> ConsultarProducts(string category)
+        {
+            using (var context = new GeneralPurposeDBEntities())
+            {
+                var resultado = (from x in context.Products
+                                 where (category == "0" ? true : x.CategoryID.ToString() == category)
+                                 select x).ToList();
+                return resultado;
+            }
+        }
+
+        public List<SelectListItem> ListaCategoryBag()
+        {
+            List<SelectListItem> ListaProducts = new List<SelectListItem>();
+            ListaProducts.Add(new SelectListItem { Value = "0", Text = "Todos" });
+            ListaProducts.Add(new SelectListItem { Value = "100", Text = "Cupcakes" });
+            ListaProducts.Add(new SelectListItem { Value = "101", Text = "Dulces" });
+            ListaProducts.Add(new SelectListItem { Value = "102", Text = "Galletas" });
+            ListaProducts.Add(new SelectListItem { Value = "103", Text = "Queques" });
+            ListaProducts.Add(new SelectListItem { Value = "104", Text = "Reposteria" });
+
+            return ListaProducts;
+        }
     }
 }
